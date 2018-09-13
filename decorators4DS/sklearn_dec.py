47c61a9e45f0a8b9a6a8c9018ccfb9484f541f89
Uri Goren <uri@goren4u.com>
2018-09-13 20:46:00 +0300

    add skclassify

--- decorators4DS/sklearn_dec.py
+++ decorators4DS/sklearn_dec.py
@@ -1,5 +1,5 @@
 import collections
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin
 
 
 class FunctionTransformer(BaseEstimator, TransformerMixin):
@@ -13,10 +13,26 @@ class FunctionTransformer(BaseEstimator, TransformerMixin):
         if isinstance(X, collections.Iterable):
             return [self.transform_func(x) for x in X]
         return self.transform_func(X)
+    
+    
+class FunctionClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, f):
+        self.predict_func = f
+    def __call__(self, X):
+        return self.predict_func(X)
+    def fit(self, X, y=None):
+        return self
+    def predict(self, X):
+        if isinstance(X, collections.Iterable):
+            return [self.predict_func(x) for x in X]
+        return self.predict_func(X)
 
 
 def sktransform(func):
     return FunctionTransformer(func)
+
+def skclassify(func):
+    return FunctionClassifier(func)
 
 if __name__ == '__main__':
     from sklearn.pipeline import Pipeline
