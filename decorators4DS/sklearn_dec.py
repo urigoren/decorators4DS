963ba5b5657fc8001a37f43050dc546aeddedc4d
Uri Goren <uri@goren4u.com>
2019-01-02 10:06:41 +0200

    Supporting pickling

--- decorators4DS/sklearn_dec.py
+++ decorators4DS/sklearn_dec.py
@@ -1,4 +1,6 @@
 import collections
+import marshal
+from types import FunctionType
 from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin
 
 
@@ -12,6 +14,20 @@ class SKTransform(BaseEstimator, TransformerMixin):
         return (i for i in [self.transform_func.__name__, self])
     def __getitem__(self, i):
         return [self.transform_func.__name__, self][i]
+    def __getstate__(self):
+        self.func_code = marshal.dumps(self.transform_func.__code__)
+        del self.transform_func
+        return self.__dict__
+    def __setstate__(self, d):
+        new_dict = {}
+        for k,v in d.items():
+            if k=='transform_func':
+                continue
+            if k=='func_code':
+                new_dict["transform_func"] = FunctionType(marshal.loads(v), globals(), "func")
+            else:
+                new_dict[k]=v
+        self.__dict__ = new_dict
     def fit(self, X, y=None):
         return self
     def transform(self, X):
@@ -30,6 +46,20 @@ class SKClassify(BaseEstimator, ClassifierMixin):
         return (i for i in [self.predict_func.__name__, self])
     def __getitem__(self, i):
         return [self.predict_func.__name__, self][i]
+    def __getstate__(self):
+        self.func_code = marshal.dumps(self.predict_func.__code__)
+        del self.predict_func
+        return self.__dict__
+    def __setstate__(self, d):
+        new_dict = {}
+        for k,v in d.items():
+            if k=='predict_func':
+                continue
+            if k=='func_code':
+                new_dict["predict_func"] = FunctionType(marshal.loads(v), globals(), "func")
+            else:
+                new_dict[k]=v
+        self.__dict__ = new_dict
     def fit(self, X, y=None):
         return self
     def fit_predict(self, X, y=None):
