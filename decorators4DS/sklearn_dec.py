945805b261d7fa9f0eb6f9edf51d62cbbf92b9e8
Uri Goren <uri@goren4u.com>
2019-01-02 11:15:02 +0200

    Update sklearn_dec.py

--- decorators4DS/sklearn_dec.py
+++ decorators4DS/sklearn_dec.py
@@ -3,73 +3,55 @@ import marshal
 from types import FunctionType
 from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin
 
-
-class SKTransform(BaseEstimator, TransformerMixin):
-    """Sklearn Transformer Decorator"""
+class SKDecorate:
     def __init__(self, f):
-        self.transform_func = f
+        self.func = f
     def __call__(self, X):
-        return self.transform_func(X)
+        return self.func(X)
     def __iter__(self):
-        return (i for i in [self.transform_func.__name__, self])
+        return (i for i in [self.func.__name__, self])
     def __getitem__(self, i):
-        return [self.transform_func.__name__, self][i]
+        return [self.func.__name__, self][i]
     def __getstate__(self):
-        self.func_name = self.transform_func.__name__
-        self.func_code = marshal.dumps(self.transform_func.__code__)
-        del self.transform_func
+        self.func_name = self.func.__name__
+        self.func_code = marshal.dumps(self.func.__code__)
+        del self.func
         return self.__dict__
     def __setstate__(self, d):
         new_dict = {}
         for k,v in d.items():
-            if k in ['transform_func', 'func_name']:
+            if k in ['func', 'func_name']:
                 continue
             if k=='func_code':
-                new_dict["transform_func"] = FunctionType(marshal.loads(v), globals(), d["func_name"])
+                new_dict["func"] = FunctionType(marshal.loads(v), globals(), d["func_name"])
             else:
                 new_dict[k]=v
         self.__dict__ = new_dict
+
+class SKTransform(BaseEstimator, TransformerMixin, SKDecorate):
+    """Sklearn Transformer Decorator"""
+    def __init__(self, f):
+        self.func = f
     def fit(self, X, y=None):
         return self
     def transform(self, X):
         if isinstance(X, collections.Iterable):
-            return [self.transform_func(x) for x in X]
-        return self.transform_func(X)
+            return [self.func(x) for x in X]
+        return self.func(X)
     
     
-class SKClassify(BaseEstimator, ClassifierMixin):
+class SKClassify(BaseEstimator, ClassifierMixin, SKDecorate):
     """Sklearn Classifier Decorator"""
     def __init__(self, f):
-        self.predict_func = f
-    def __call__(self, X):
-        return self.predict_func(X)
-    def __iter__(self):
-        return (i for i in [self.predict_func.__name__, self])
-    def __getitem__(self, i):
-        return [self.predict_func.__name__, self][i]
-    def __getstate__(self):
-        self.func_name = self.predict_func.__name__
-        self.func_code = marshal.dumps(self.predict_func.__code__)
-        del self.predict_func
-        return self.__dict__
-    def __setstate__(self, d):
-        new_dict = {}
-        for k,v in d.items():
-            if k in ['predict_func', 'func_name']:
-                continue
-            if k=='func_code':
-                new_dict["predict_func"] = FunctionType(marshal.loads(v), globals(), d["func_name"])
-            else:
-                new_dict[k]=v
-        self.__dict__ = new_dict
+        self.func = f
     def fit(self, X, y=None):
         return self
     def fit_predict(self, X, y=None):
         return self.predict(X)
     def predict(self, X, y=None):
         if isinstance(X, collections.Iterable):
-            return [self.predict_func(x) for x in X]
-        return self.predict_func(X)
+            return [self.func(x) for x in X]
+        return self.func(X)
 
 
 if __name__ == '__main__':
