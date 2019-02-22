d8353f8a665948c1af7fb49817d27a374c300cbe
Uri Goren <uri@goren4u.com>
2019-02-22 16:34:39 +0200

    Using `cloudpickle`

--- decorators4DS/sklearn_dec.py
+++ decorators4DS/sklearn_dec.py
@@ -1,48 +1,59 @@
 import collections
-import marshal
-from types import FunctionType
+import cloudpickle
 from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin
+
 
 class SKDecorate:
     def __init__(self, f):
         self.func = f
+
     def __call__(self, X):
         return self.func(X)
+
     def __iter__(self):
         return (i for i in [self.func.__name__, self])
+
     def __getitem__(self, i):
         return [self.func.__name__, self][i]
+
     def __getstate__(self):
-        self.func_name = self.func.__name__
-        self.func_code = marshal.dumps(self.func.__code__)
+        self.func_pkl = cloudpickle.dumps(self.func)
         del self.func
         return self.__dict__
+
     def __setstate__(self, d):
-        d["func"] = FunctionType(marshal.loads(d["func_code"]), globals(), d["func_name"])
-        del d["func_name"]
-        del d["func_code"]
+        d["func"] = cloudpickle.loads(d["func_pkl"])
+        del d["func_pkl"]
         self.__dict__ = d
+
 
 class SKTransform(BaseEstimator, TransformerMixin, SKDecorate):
     """Sklearn Transformer Decorator"""
+
     def __init__(self, f):
         self.func = f
+
     def fit(self, X, y=None):
         return self
+
     def transform(self, X):
         if isinstance(X, collections.Iterable):
             return [self.func(x) for x in X]
         return self.func(X)
-    
-    
+
+
 class SKClassify(BaseEstimator, ClassifierMixin, SKDecorate):
     """Sklearn Classifier Decorator"""
+
     def __init__(self, f):
         self.func = f
+
     def fit(self, X, y=None):
         return self
+
     def fit_predict(self, X, y=None):
         return self.predict(X)
+
     def predict(self, X, y=None):
         if isinstance(X, collections.Iterable):
             return [self.func(x) for x in X]
@@ -52,15 +63,27 @@ class SKClassify(BaseEstimator, ClassifierMixin, SKDecorate):
 if __name__ == '__main__':
     # Toy usage example:
     from sklearn.pipeline import Pipeline
+    from sklearn.externals import joblib
+
+
     @SKTransform
     def power2(x):
-        return x**2
+        return x ** 2
+
+
     @SKClassify
     def lessThan50(x):
-        return x<50
+        return x < 50
 
-    ppl=Pipeline([
-                  power2,
-                  lessThan50,
-                 ])
-    assert ppl.predict([3,6,8,10]) == [True, True, False, False]
+
+    ppl = Pipeline([
+        power2,
+        lessThan50,
+    ])
+    assert ppl.predict([3, 6, 8, 10]) == [True, True, False, False]
+    joblib.dump(ppl, "ppl.pkl")
+    del power2
+    del lessThan50
+    del ppl
+    ppl = joblib.load("ppl.pkl")
+    assert ppl.predict([3, 6, 8, 10]) == [True, True, False, False]
